968c3ec6b245575e24abeba233d3d7011f5849eb
mikawill <60400255+mikawill@users.noreply.github.com>
2020-02-04 09:43:11 -0600

    Updated Volunteer info and created templates
    Update templates and added test code to check if templates are working.

---
+++ volunteerEPICS/volunteers/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('sign-in/', views.signin, name='sign-in'), #links to /volunteers/sign-in
+    path('sign-up/', views.signup, name='sign-up'), #links to /volunteers/sign-up
+    path('sign-out/', views.signout, name='sign-out'), #links to /volunteers/sign-out
+    path('thanks/', views.thanks, name='thanks'),  #links to /volunteers/thanks
+    path('', views.volunteers, name='volunteershome'), #links to /volunteers (as indecaded by the '')
+]
--- volunteerEPICS/volunteers/views.py
+++ volunteerEPICS/volunteers/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def signin(request):  #signin page defenition
+    return render(request,'volunteers/signin.html' ) #sends Django to templates for signin.html
+
+def signup(request):  #signup page defenition
+        return HttpResponse('<h1>sign up page</h1>')
+
+def thanks(request):   #thank you page defenition
+    return HttpResponse('<h1>Thank you for volunteering (making magic!)</h1>')
+
+def signout(request):
+    return HttpResponse('<h1>sign out page hi ivana</h1>')  #sign out page defenition
+
+def volunteers(request):
+    return HttpResponse('<h1>home page for volunteers</h1>')  #defenition for just /volunteers
