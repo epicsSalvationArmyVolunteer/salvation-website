51f00dbae2ce95c408b27933e3ad791e900eddf7
mikawill <60400255+mikawill@users.noreply.github.com>
2020-04-23 14:01:02 -0500

    Added a forms.py file for Django form
    Also made changes to admin.py, models.py and views.py

--- volunteerEPICS/volunteers/apps.py
+++ volunteerEPICS/volunteers/apps.py
@@ -1,5 +1,26 @@
 from django.apps import AppConfig
+#from volunteerEPICS.volunteers.models import Volunteer
 
 
 class VolunteersConfig(AppConfig):
     name = 'volunteers'
+
+
+def volunteer_login(volunteer, event):
+    """Call this function to "login" a volunteer to an event"""
+    event.add(volunteer)
+
+
+def volunteer_hours(volunteer, number_of_events):
+    """Return the number of hours in the events in the specified range"""
+    """The range is an integer greater than one, and refers to events from most to least recent"""
+    if number_of_events <= 0:
+        return "Bad range, range must be a positive integer"
+    return volunteer.events.all.filter()
+
+
+def all_employees_of_company(company):
+    """
+    Return all volunteers whose employer is company
+    """
+    return Volunteer.objects.filter(employer=company)
